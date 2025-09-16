479faffe3a72e0ae7f006aa62e20254870f9c300
hiraygui <hiraygui@gmail.com>
2025-09-16 12:49:55 -0300

    feat(DashboardItem): Criando Component reutilizável e implementando-o

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header';
 import { ServerStatusComponent } from './dashboard/server-status/server-status';
 import { TrafficComponent } from './dashboard/traffic/traffic';
 import { TicketsComponent } from './dashboard/tickets/tickets';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent ],
+  imports: [ HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent ],
   templateUrl: './app.html',
 })
 export class AppComponent {
