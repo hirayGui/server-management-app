538b83d7de55b81efab4b538ef9f56455df9361c
hiraygui <hiraygui@gmail.com>
2025-11-24 17:07:09 -0300

    feat(ServerStatus): Implementando LifeCycle Component ngOnInit para executar pequeno trecho que simula mudança de status do servidor

--- src/app/dashboard/server-status/server-status.ts
+++ src/app/dashboard/server-status/server-status.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,8 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.html',
   styleUrl: './server-status.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit{
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
   
+  constructor(){}
+
+  ngOnInit(){
+    // Criando um intervalo para simular mudanças de status do servidor
+    setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.9999999999999999
+
+      if(rnd < 0.5){
+        this.currentStatus = 'online';
+      }else if(rnd < 0.9){
+        this.currentStatus = 'offline';
+      }else{
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
