6e65abc05c1674f1d1a935009ed895034d646988
hiraygui <hiraygui@gmail.com>
2026-03-17 19:59:31 -0300

    feat(NewTicket): Recebe dados do formulário utilizando Template Variable

--- src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import { ButtonComponent } from "../../../shared/button/button";
 import { ControlComponent } from "../../../shared/control/control";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css'
 })
 export class NewTicketComponent {
 
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  onSubmit(title: string, ticketText: string) {
+    console.log('Title: ', title);
+    console.log('Ticket Text: ', ticketText);
+    this.form?.nativeElement.reset(); // reseta o formulário após o submit
+  }
 }
